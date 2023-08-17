09cdee4de12731f9202d9f035dda14ffa51d80f7
Camila Lo Nero <lonerocamila@gmail.com>
2023-08-17 15:00:36 -0300

    config.ts jwt

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,18 @@
 import express from 'express'
 import { initializate } from './server'
 
+//initializations
 const app = express()
 
 await initializate(app)
+
+
+//settings
+
+
+//middlewares
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+//routes
+
+
