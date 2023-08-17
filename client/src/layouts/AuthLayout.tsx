c3ff80e445b1b3860ae429ef5abd649ef21afb82
John Pablo <john.davis.2015.03@gmail.com>
2023-08-17 18:51:45 -0500

    Add/ Instalar configuración y dependencias faltantes: Tailwindcss, React router

---
+++ client/src/layouts/AuthLayout.tsx
@@ -0,0 +1,8 @@
+export const AuthLayout = () => {
+  return (
+    <div className="bg-red-400 text-center p-8 text-white">
+      <h1 className="font-bold">Soy el Layout 🎈 No Country 2023</h1>
+      <h3>Pensando en como Dividir el Layout y que sea Responsive...</h3>
+    </div>
+  );
+};
