d086110009a10e90fd3895c2a55431909e1d45a6
Camila Lo Nero <lonerocamila@gmail.com>
2023-08-23 16:57:39 -0300

    user CRUD

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
-import { Response, Request, NextFunction } from 'express'
+import { Response, Request, NextFunction, response } from 'express'
 import { userService } from '../services/factory/entities.factory'
 import { AppError } from '../utils/app.error'
-import type { User } from '../entities/'
+import { User } from '../entities/'
 import type { Login } from '../types/user.types'
 
 export const signUp = async (
@@ -44,3 +44,136 @@ export const singIn = async (
     next(err)
   }
 }
+
+export const singIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { token, user } = await userService.signIn(
+      req.safeData?.body as Login
+    )
+
+    return res.status(200).json({
+      status: 'success',
+      token,
+      user
+    })
+  } catch (err) {
+    if (!(err instanceof AppError))
+      return next(new AppError('No se pudo autenticar el usuario.', 500))
+    next(err)
+  }
+}
+
+
+
+
+//crear un nuevo usuario
+
+export const createUser = async (
+  req: Request, 
+  res: Response,
+  next: NextFunction
+  ) =>{
+  try {
+      const {firstName, lastName, email} = req.body
+
+      const user = new User()
+      user.firstName = firstName;
+      user.lastName = lastName;
+      user.email = email
+
+      await user.save();
+
+      return res.json(user)
+  } catch (err) {
+    if (!(err instanceof AppError))
+      return next(new AppError('No se pudo autenticar el usuario.', 500))
+    next(err)   
+  }
+
+}
+
+
+
+//Leer los usuarios 
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+  ) =>{
+
+  try {
+    const users = await  User.find()
+    return res.json(users)
+
+  } catch (err) {
+    if (!(err instanceof AppError))
+      return next(new AppError('No se pudo autenticar el usuario.', 500))
+    next(err)  
+  }
+}
+
+
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) =>{
+ try {
+  const {id} = req.params;
+
+  const user = await User.findOneBy({ id: parseInt(req.params.id)});
+
+  if (!user) return res.status(404).json({message: "User does not exists"})
+
+  await User.update({ id: parseInt(id)}, req.body)
+
+ } catch (err) {
+  if (!(err instanceof AppError))
+   return next(new AppError('No se pudo autenticar el usuario.', 500))
+  next(err)  
+ }
+
+}
+
+export const deleteUser = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) =>{
+  try {
+    const { id } = req.params;
+    const result = await User.delete({ id: parseInt(id)})
+    
+    if (result.affected === 0) {
+    return res.status(404).json({message: "User does not exists"})
+
+    }
+    return res.sendStatus(204)
+  } catch (err) {
+    if (!(err instanceof AppError))
+      return next(new AppError('No se pudo autenticar el usuario.', 500))
+    next(err)
+  }
+}
+
+export const getUser = async (
+  req: Request, 
+  res: Response,
+  next: NextFunction
+  )=>{ 
+   try {
+    const {id} = req.params
+    const user = await User.findOneBy({id: parseInt(id)})
+    return res.json(user)
+
+   } catch (err) {
+    if (!(err instanceof AppError))
+      return next(new AppError('No se pudo autenticar el usuario.', 500))
+    next(err)
+   }
+  } 
+
--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { signUp, singIn } from '../controllers/user.controller'
+import { signUp, singIn, createUser, getUsers, updateUser } from '../controllers/user.controller'
 import { schemaValidator } from '../middlewares/schema.middleware'
 import { loginSchema } from '../schema/login.schema'
 import { userSchema } from '../schema/user.schema'
@@ -8,3 +8,6 @@ export const authRouter = Router()
 
 authRouter.post('/signup', schemaValidator(userSchema), signUp)
 authRouter.post('/signin', schemaValidator(loginSchema), singIn)
+authRouter.post('/users', createUser)
+authRouter.get('users', getUsers )
+authRouter.put('/users/:id', updateUser)
