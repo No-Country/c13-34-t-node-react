98d100cc5f1cd9eb50736ab1f15771d5e8ed3885
juancgalueweb <juancgalue@icloud.com>
2023-08-25 20:37:43 -0400

    feat: :sparkles: Protege la ruta para obtener doctores y admins
    Solo los admin que estén habilitados pueden hacer el get de todos los demás admin y doctores

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,6 +1,13 @@
 import { Router } from 'express'
 import { getAllDoctorsAndAdmins } from '../controllers/admin.controller'
+import { protect, restrictTo } from '../middlewares/auth.middleware'
+import { UserRole } from '../types/user.types'
 
 export const adminRouter = Router()
 
-adminRouter.get('/high-level-roles', getAllDoctorsAndAdmins)
+adminRouter.get(
+  '/high-level-roles',
+  protect,
+  restrictTo(UserRole.admin),
+  getAllDoctorsAndAdmins
+)
