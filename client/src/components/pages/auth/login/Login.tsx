c37f90b21f0dbee58e61ecb2d4b1bca3f479c69c
John Pablo <john.davis.2015.03@gmail.com>
2023-08-26 21:55:52 -0500

    se agrego el responsive Mobile al Login

--- client/src/components/pages/auth/login/Login.tsx
+++ client/src/components/pages/auth/login/Login.tsx
@@ -37,9 +37,9 @@ export const Login = () => {
   };
 
   return (
-    <div className="h-full py-20 pl-0 pr-36 bg-white">
-      <div className="grid grid-cols-2 gap-24">
-        <div className="flex flex-col justify-center items-center relative">
+    <div className="h-full w-full px-4 pt-0 pb-8 2xl:py-20 2xl:pl-0 2xl:pr-36 bg-white">
+      <div className="grid grid-cols-1 2xl:grid-cols-2 gap-24">
+        <div className="hidden 2xl:flex flex-col justify-center items-center relative">
           <div className="absolute">
             <img
               src="/images/circles.png"
@@ -58,20 +58,23 @@ export const Login = () => {
           </div>
         </div>
 
-        <div className="w-[502px] flex flex-col font-opensans">
-          <h1 className="font-caudex text-primary-green text-5xl pb-4">
+        <div className="w-full 2xl:w-[502px] flex flex-col font-opensans">
+          <h1 className="font-caudex text-primary-green text-5xl 2xl:text-6xl pb-4">
             Acceso
           </h1>
           <div className="pb-8">
-            <h1 className="text-2xl text-primary-green font-bold">
+            <h1 className="text-2xl text-primary-green font-bold tracking-[1px]">
               ¡Estamos felices de verte!
             </h1>
-            <p className="text-lg text-dark-green font-bold">
+            <p className="text-lg text-dark-green font-bold tracking-[1px]">
               Inicia sesión con tus datos
             </p>
           </div>
 
-          <form onSubmit={onSubmit} className="w-[360px] flex flex-col gap-6">
+          <form
+            onSubmit={onSubmit}
+            className="w-full 2xl:w-[360px] flex flex-col gap-6 max-sm:pb-4"
+          >
             <label className="block">
               <span className="block">Correo Electrónico *</span>
               <input
@@ -90,25 +93,25 @@ export const Login = () => {
                 name="password"
                 required
                 placeholder="Ingrese su contraseña"
-                className="ring-1 ring-gray-300 w-full rounded-xl px-4 py-3 mt-2 outline-none focus:ring-2 focus:ring-primary-gray"
+                className="ring-1 ring-gray-300 w-full rounded-xl px-4 py-3 mt-2 mb-0 outline-none focus:ring-2 focus:ring-primary-gray"
               />
             </label>
             <button
               disabled={loading}
               type="submit"
-              className="w-[360px] mt-4 py-3 rounded-3xl text-xl text-white hover:text-primary-green bg-primary-green hover:bg-white border-primary-green border transition duration-300 disabled:bg-dark-green"
+              className="w-full 2xl:w-[360px] mt-4 py-2 rounded-xl text-xl text-white hover:text-primary-green bg-primary-green hover:bg-white border-primary-green border transition duration-300 disabled:bg-dark-green"
             >
               Ingresar
             </button>
           </form>
-          <div className="w-[360px] pt-2 grid grid-cols-2 justify-between text-primary-green">
+          <div className="w-full 2xl:w-[360px] pt-2 grid grid-cols-2 justify-between text-primary-green">
             <NavLink to="/recuperar-contrasena">
               ¿Has olvidado tu contraseña?
             </NavLink>
 
             <div className="flex flex-col text-center">
               <NavLink to="/registro">
-                ¿No tienes una cuenta? Regístrate ahora*
+                ¿No tienes una cuenta? Regístrate ahora *
               </NavLink>
             </div>
           </div>
