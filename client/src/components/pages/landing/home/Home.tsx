7e92270ac724b281acf65285ce34264d84ac6ebf
paulitab22 <paulitabpublicidad@gmail.com>
2023-08-28 20:23:38 -0300

    fix

--- client/src/components/pages/landing/home/Home.tsx
+++ client/src/components/pages/landing/home/Home.tsx
@@ -1,8 +1,9 @@
 import { Attention } from "./shared/Attention";
 import { MedicalSpecialties } from "./shared/MedicalSpecialties";
+import { Services } from "./shared/Services";
 import { Testimonials } from "./shared/Testimonials";
 import { Welcome } from "./shared/Welcome";
-import { Services } from "./shared/Services";
+
 
 export const Home = () => {
   return (
@@ -10,12 +11,15 @@ export const Home = () => {
       <section className="p-4 mb-32 2xl:pt-20 2xl:px-36 bg-gradient-to-r from-yellow-50 to-green-100">
         <Welcome />
       </section>
-
-
       <section className="py-20 px-36">
         <Attention/>
       </section>
-
+      <section className=" py-12 mb-40">
+        <Services/>
+      </section>
+      <section className="overflow-auto mb-40">
+        <Testimonials/>
+      </section>
       <section className="py-4 px-36">
         <MedicalSpecialties />
       </section>
