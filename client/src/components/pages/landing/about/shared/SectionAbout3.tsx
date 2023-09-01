d12994666d1ab8322383f72890acdf00fc02d64f
OLuciani <lucianioscar1@gmail.com>
2023-09-01 11:26:53 -0300

    Ajustes con estilos responsivos de tailwind css en el componente SectionAbout3.tsx (adaptación a mobile)

--- client/src/components/pages/landing/about/shared/SectionAbout3.tsx
+++ client/src/components/pages/landing/about/shared/SectionAbout3.tsx
@@ -1,125 +1,4 @@
-/* export const SectionAbout3 = () => {
-    return (
-      <div className="flex-col items-center md:flex-row grid mt-12">
-        <div className="text-center">
-            <div>
-                <h1 className="text-3xl font-bold mb-4">Qué hace que Meddyplus se destaque como una clínica de alta tecnología ?</h1>
-            </div>
-            <div>
-                <h3 className="text-primary-green text-2xl font-bold">Primero y único con el equipamiento de atención más avanzado y un gran número de especialistas con mucha experiencia bajo un mismo techo.</h3>
-            </div>
-        </div>
-
-        <div className="justify-self-center mt-10">
-            <div className="flex-col items-center md:flex md-gap-40 mb-16">
-                <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                    <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                    <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 50.000 tratamientos exitosos.</p>
-                </div>
-
-                <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                    <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                    <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 3.000 cirugías 3D exitosas.</p>
-                </div>
-
-                <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                    <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                    <p className="pl-3 pr-3 mt-32 text-center relative z-10">Primero en el país en imágenes digitales avanzadas.</p>
-                </div>
-            </div>
-
-            <div className="flex-col items-center md:flex md-gap-40">
-                <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                    <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                    <p className="pl-3 pr-3 mt-32 text-center relative z-10">Primera y única cirugía de retina vitro 3D.</p>
-                </div>
-
-                <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                    <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                    <p className="pl-3 pr-3 mt-32 text-center relative z-10">Angiografía OCT.</p>
-                </div>
-
-                <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                    <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                    <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 10.000 cirugías exitosas.</p>
-                </div>
-            </div>
-
-        </div>
-      </div>
-    );
-  }; */
-  
-
-
-
-  /* export const SectionAbout3 = () => {
-    return (
-         <div className=" mt-12">
-            <div className="text-center">
-                <div>
-                    <h1 className="text-3xl font-bold mb-4">Qué hace que Meddyplus se destaque como una clínica de alta tecnología ?</h1>
-                </div>
-                <div>
-                    <h3 className="text-primary-green text-2xl font-bold">Primero y único con el equipamiento de atención más avanzado y un gran número de especialistas con mucha experiencia bajo un mismo techo.</h3>
-                </div>
-            </div>
-
-              
-          
-            <div className="">
-                <div className="flex-row justify-around">
-                    <div className="w-72 h-56 mb-8 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden ">
-                      <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                      <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 50.000 tratamientos exitosos.</p>
-                    </div>
-
-                    <div className="w-72 h-56 mb-8 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                      <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                      <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 3.000 cirugías 3D exitosas.</p>
-                    </div>
-
-                    <div className="w-72 h-56 mb-8 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                      <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                      <p className="pl-3 pr-3 mt-32 text-center relative z-10">Primero en el país en imágenes digitales avanzadas.</p>
-                    </div>
-                
-
-                
-                    <div className="w-72 h-56 mb-8 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                      <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                      <p className="pl-3 pr-3 mt-32 text-center relative z-10">Primera y única cirugía de retina vitro 3D.</p>
-                    </div>
-
-                    <div className="w-72 h-56 mb-8 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                      <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                      <p className="pl-3 pr-3 mt-32 text-center relative z-10">Angiografía OCT.</p>
-                    </div>
-
-                    <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
-                      <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
-
-                      <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 10.000 cirugías exitosas.</p>
-                    </div>
-                </div>
-            </div>
-          </div>    
-    );
-  }; */
-  
-
-  export const SectionAbout3 = () => {
+export const SectionAbout3 = () => {
     return (
       <div className="flex flex-col mt-12">
         <div className="text-center mb-6">
@@ -127,35 +6,35 @@
           <h3 className="text-primary-green text-2xl font-bold">Primero y único con el equipamiento de atención más avanzado y un gran número de especialistas con mucha experiencia bajo un mismo techo.</h3>
         </div>
   
-        <div className="flex flex-col gap-8 md:gap-12 md:flex-row md:justify-evenly md:mb-12 mt-8 mb-8">
-          <div className="w-full md:w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
+        <div className="flex flex-col items-center gap-8 2xl:gap-12 2xl:flex-row 2xl:justify-evenly 2xl:mb-12 mt-8 mb-8">
+          <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
             <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
             <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 50.000 tratamientos exitosos.</p>
           </div>
   
-          <div className="w-full md:w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
+          <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
             <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
             <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 3.000 cirugías 3D exitosas.</p>
           </div>
   
-          <div className="w-full md:w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
+          <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
             <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
             <p className="pl-3 pr-3 mt-32 text-center relative z-10">Primero en el país en imágenes digitales avanzadas.</p>
           </div>
         </div>
   
-        <div className="flex flex-col gap-8 md:gap-12 md:flex-row md:justify-evenly">
-          <div className="w-full md:w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
+        <div className="flex flex-col items-center gap-8 md:gap-12 md:flex-row md:justify-evenly">
+          <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
             <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
             <p className="pl-3 pr-3 mt-32 text-center relative z-10">Primera y única cirugía de retina vitro 3D.</p>
           </div>
   
-          <div className="w-full md:w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
+          <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
             <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
             <p className="pl-3 pr-3 mt-32 text-center relative z-10">Angiografía OCT.</p>
           </div>
   
-          <div className="w-full md:w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
+          <div className="w-72 h-56 border-primary-green border-2 rounded-xl shadow-xl relative overflow-hidden">
             <div className="absolute w-[500px] h-[400px] rounded-[60%] bg-lightest-green bottom-4 right-4"></div>
             <p className="pl-3 pr-3 mt-32 text-center relative z-10">Más de 10.000 cirugías exitosas.</p>
           </div>
