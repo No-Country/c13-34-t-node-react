f7d391260ba24d4b67c253c0142073e6264cbbd2
Camila Lo Nero <lonerocamila@gmail.com>
2023-09-04 19:04:56 -0300

    get tabla de pacientes

--- server/src/services/patient.service.ts
+++ server/src/services/patient.service.ts
@@ -1,14 +1,18 @@
 import type { Patient } from '../entities'
 import type { PatientRepository } from '../types/patient.types'
 import { EntityService } from './entity.service'
+import type { MedicalAppointmentService } from './medical.appointment.service'
 
 export class PatientService {
-  //private readonly patientRepository: PatientRepository
+  // private readonly patientRepository: PatientRepository
   private readonly entityService: EntityService
 
   constructor(paientRepository: PatientRepository) {
-    //this.patientRepository = paientRepository
+    // this.patientRepository = paientRepository
     this.entityService = new EntityService(paientRepository)
+  }
+  async getmedicalappointment(patient: MedicalAppointmentService) {
+   this.getPatient(patient)) as patient
   }
 
   async createPatient(patient: Patient): Promise<Patient> {
