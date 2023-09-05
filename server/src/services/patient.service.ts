81b1d352c21e4e0a40990d50af0d080d9f7384b5
Camila Lo Nero <lonerocamila@gmail.com>
2023-09-05 16:56:16 -0300

    /medical-appointment/

--- server/src/services/patient.service.ts
+++ server/src/services/patient.service.ts
@@ -1,8 +1,7 @@
 import type { Patient } from '../entities/patient.entity'
 import type { PatientRepository } from '../types/patient.types'
 import { EntityService } from './entity.service'
-import type { MedicalAppointmentService } from './medical.appointment.service'
-import { UserRole } from '../types/user.types'
+
 export class PatientService {
   // private readonly patientRepository: PatientRepository
   private readonly entityService: EntityService
